bd04878290e7095a54cb0dae338bdb8b8a867357
Michele Braccini <miki892@gmail.com>
2018-01-10 16:16:47 +0100

    test della funzione che riordina la matrice in base ai valori presenti dulla diagonale

---
+++ src/test/java/TestMatrixUtility.java
@@ -0,0 +1,47 @@
+import org.junit.Test;
+import utility.GenericUtility;
+import utility.MatrixUtility;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestMatrixUtility {
+
+    /**
+     * From this matrix:
+     *
+     * 0.7  0.3  0.0
+     * 0.1  0.1  0.8
+     * 0.0  0.4  0.6
+     *
+     * We must obtain:
+     *
+     * 0.1  0.8  0.1
+     * 0.4  0.6  0.0
+     * 0.3  0.0  0.7
+     *
+     */
+    @Test
+    public void TestReorderByDiagonalValues() {
+
+        Double[][] matrix = {
+                {0.7,  0.3,  0.0},
+                {0.1,  0.1,  0.8},
+                {0.0,  0.4,  0.6}
+        };
+
+        Number[][] m = MatrixUtility.reorderByDiagonalValues(matrix);
+
+        GenericUtility.printMatrix(m);
+
+        assertTrue("elm [0][0] non corretto", m[0][0].doubleValue() == 0.1);
+        assertTrue("elm [0][1] non corretto", m[0][1].doubleValue() == 0.8);
+        assertTrue("elm [0][2] non corretto", m[0][2].doubleValue() == 0.1);
+        assertTrue("elm [1][0] non corretto", m[1][0].doubleValue() == 0.4);
+        assertTrue("elm [1][1] non corretto", m[1][1].doubleValue() == 0.6);
+        assertTrue("elm [1][2] non corretto", m[1][2].doubleValue() == 0.0);
+        assertTrue("elm [2][0] non corretto", m[2][0].doubleValue() == 0.3);
+        assertTrue("elm [2][1] non corretto", m[2][1].doubleValue() == 0.0);
+        assertTrue("elm [2][2] non corretto", m[2][2].doubleValue() == 0.7);
+
+    }
+}
