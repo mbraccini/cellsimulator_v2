29a44d6a68273044d23461568045fafb5fd02f26
Michele <miki892@gmail.com>
2020-03-10 15:51:09 +0100

    esperimenti specificIslands

--- src/main/java/experiments/frozenSea/TwinklingIslandsByPerturbation.java
+++ src/main/java/experiments/frozenSea/TwinklingIslandsByPerturbation.java
@@ -14,6 +14,7 @@ import interfaces.network.NodeDeterministic;
 import interfaces.sequences.Generator;
 import interfaces.state.BinaryState;
 import io.vavr.Tuple2;
+import io.vavr.Tuple3;
 import network.BooleanNetworkFactory;
 import org.apache.commons.math3.random.RandomGenerator;
 import simulator.AttractorsFinderService;
@@ -25,40 +26,81 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.math.BigInteger;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class TwinklingIslandsByPerturbation {
 
     private static final int MAX_STEPS_TO_FIND_ATTRS = 10000; //serve perché potrei non trovarmi in uno degli attrattori trovati con la ricerca incompleta, senza di questa controllerebbe all'infinito
     private static final int INIT_SAMPLES = 1000;
-    private static final int NUM_OF_BNS = 100;
+    private static final int NUM_OF_BNS = 30;
 
     public static void main(String [] args){
         System.out.println("TWINK-Perturbations");
+        randomBN();
+        //cellcollective();
 
+       /* String str = "/cellcollective/bbb.txt";
+        Pattern p = Pattern.compile("\\/cellcollective\\/(.*?).txt");
+        Matcher m = p.matcher(str);
+
+        String a = m.results().findFirst().get().group(1);
+        System.out.println(a);
+            //m.results().forEach(x -> System.out.println(x.group(1))); //is your string. do what you want
+        */
+    }
+
+    public static void randomBN(){
         RandomGenerator r = RandomnessFactory.getPureRandomGenerator();
         String folder = "twinkling" + Files.FILE_SEPARATOR;
         Files.createDirectories(folder);
         final int nodesNumber = 100;
         final int k = 2;
         final double bias = 0.5;
-        IntStream.range(0, NUM_OF_BNS).forEach(idBN -> analiseNet(folder, idBN, nodesNumber,k,bias, r, Boolean.FALSE));
+        IntStream.range(0, NUM_OF_BNS).forEach(
+                idBN -> { BNClassic<BitSet, Boolean, NodeDeterministic<BitSet, Boolean>> bn =
+                        BooleanNetworkFactory.newRBN(BNKBias.BiasType.CLASSICAL, BooleanNetworkFactory.SelfLoop.WITHOUT, nodesNumber, k, bias, r);
+                    analiseNet(bn,folder, idBN, r, Boolean.FALSE);});
+    }
+    public static void cellcollective(){
+        Pattern p = Pattern.compile("\\/cellcollective\\/(.*?)_v1.txt");
+        RandomGenerator r = RandomnessFactory.getPureRandomGenerator();
+        String folder = "twinkling_cellcollective" + Files.FILE_SEPARATOR;
+        Files.createDirectories(folder);
+        try (Stream<Path> paths = java.nio.file.Files.walk(Paths.get("./cellcollective/"))) {
+            paths
+                    .filter(java.nio.file.Files::isRegularFile)
+                    .filter(x -> x.toString().endsWith(".txt"))
+                    .forEach(x -> {
+                        Matcher m = p.matcher(x.toString());
+                        String numero_rete = m.results().findFirst().get().group(1);
+                        System.out.println(x);
+                        BNClassic<BitSet, Boolean, NodeDeterministic<BitSet, Boolean>> bn = BooleanNetworkFactory.newNetworkFromFile(x.toString());
+                        analiseNet(bn,folder,Integer.valueOf(numero_rete),r, Boolean.FALSE);
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void analiseNet(  final String folder,
+    public static void analiseNet(  final BNClassic<BitSet, Boolean, NodeDeterministic<BitSet, Boolean>> bn,
+                                    final String folder,
                                     final int idBN,
-                                    final int nodesNumber,
-                                    final int k,
-                                    final double bias,
                                     final RandomGenerator r,
                                     final Boolean completeExploration){
 
-        BNClassic<BitSet, Boolean, NodeDeterministic<BitSet, Boolean>> bn;
-        bn = BooleanNetworkFactory.newRBN(BNKBias.BiasType.CLASSICAL, BooleanNetworkFactory.SelfLoop.WITHOUT, nodesNumber, k, bias, r);
-
-        Attractors<BinaryState> atts = getAttractors(bn, completeExploration, r);
+        Attractors<BinaryState> atts = getAttractors(bn,
+                                                completeExploration,
+                                                r);
+                //(completeExploration.equals(Boolean.TRUE) ? AttractorsFinderService.TRUE_TERMINATION : AttractorsFinderService.CUT_OFF_PERCENTAGE_TERMINATION.apply(bn.getNodesNumber()))
+                 //                   );
 
         //System.out.println("atts\n"+atts);
         Set<Integer> fix = AttractorsUtility.fixedNodesAllAttractors(atts);
@@ -70,6 +112,7 @@ public class TwinklingIslandsByPerturbation {
         Files.writeBooleanNetworkToFile(bn, folder + idBN +  "_bn");
         Files.writeAttractorsToReadableFile(atts, folder + idBN + "_attrs");
         new BNGraphViz<>(bn).saveOnDisk(folder + idBN +  "_bn_dot");
+        Files.writeListsToCsv(List.of(new ArrayList<>(blink)), folder + idBN +  "_blinking.csv");
         perturb(folder, idBN,bn, atts, blink, r);
     }
 
@@ -86,10 +129,12 @@ public class TwinklingIslandsByPerturbation {
             for (Integer toFlip: blink) {
                 //System.out.println("toFlip: "+toFlip);
                 for (ImmutableAttractor<BinaryState> a : atts.getAttractors()) {
+                    System.out.println("attractorStart:" + a.getId());
+
                     for (BinaryState s : a.getStates()) {
                         //System.out.println("stato: "+s);
                         BinaryState startState = s.flipNodesValues(toFlip);
-                        Tuple2<List<BinaryState>,List<BinaryState>> traj_attr = untilAttractor(bn, startState, atts);
+                        Tuple3<List<BinaryState>,List<BinaryState>,Integer> traj_attr = untilAttractor(bn, startState, atts);
 
                         if (traj_attr != null){ //not reached known attractors in MAX_STEPS_TO_FIND_ATTRS
                             /*
@@ -98,8 +143,15 @@ public class TwinklingIslandsByPerturbation {
                             System.out.println("attra");
                             traj_attr._2.stream().forEach(System.out::println);
                             */
+                            Integer id_attrattore_finale_trovato = traj_attr._3();
                             List<Integer> traj = findDifferences(startState,traj_attr._1(), blink, toFlip);//diff in transient's states
-                            List<Integer> attr = findDifferences(startState,traj_attr._2(), blink, toFlip);//differences in attractors's states
+                            List<Integer> attr;
+                            if(id_attrattore_finale_trovato.equals(a.getId())){ //se è uguale a quello iniziale le differenze sono 0
+                                System.out.println("Attrattore finale e inziale uguali");
+                                attr = new ArrayList<>();
+                            } else {
+                                attr = findDifferences(startState, traj_attr._2(), blink, toFlip);//differences in attractors's states
+                            }
                             traj.add(0, a.getId());
                             attr.add(0, a.getId());
                             traj.add(0, toFlip);
@@ -147,7 +199,7 @@ public class TwinklingIslandsByPerturbation {
      * @param atts
      * @return
      */
-    private static Tuple2<List<BinaryState>,List<BinaryState>> untilAttractor(final BNClassic<BitSet, Boolean, NodeDeterministic<BitSet, Boolean>> bn,
+    private static Tuple3<List<BinaryState>,List<BinaryState>, Integer> untilAttractor(final BNClassic<BitSet, Boolean, NodeDeterministic<BitSet, Boolean>> bn,
                                                            final BinaryState start,
                                                            final Attractors<BinaryState> atts){
         List<BinaryState> traj = new ArrayList<>();
@@ -164,7 +216,11 @@ public class TwinklingIslandsByPerturbation {
             if (traj.size() > 0){
                 traj.remove(traj.size() - 1);
             }
-            return new Tuple2<>(traj,AttractorsUtility.retrieveAttractor(previous, atts.getAttractors()).get().getStates());
+            Integer attractorFound = AttractorsUtility.retrieveAttractorId(previous, atts.getAttractors());
+            System.out.println("attractorFound:" + attractorFound);
+            return new Tuple3<>(traj,AttractorsUtility.retrieveAttractor(previous,
+                    atts.getAttractors()).get().getStates(),
+                    attractorFound);
         }
         return null;
 
@@ -177,7 +233,8 @@ public class TwinklingIslandsByPerturbation {
 
     private static Attractors<BinaryState> getAttractors(final BNClassic<BitSet, Boolean, NodeDeterministic<BitSet, Boolean>> bn,
                                                          final Boolean completeExploration,
-                                                         final RandomGenerator r) {
+                                                         final RandomGenerator r
+                                                         ) {
         Generator<BinaryState> gen;
         Dynamics<BinaryState> dyn;
         if (completeExploration){
